2ef1680b60262bc3e1d48d55b04cb933c8db0f7b
Ivan <tikerlade@gmail.com>
2022-04-06 02:37:12 +0300

    Solutions for all graph problems now are HashSet<Integer>.

--- src/main/java/ru/spbstu/icst/problems/Clique.java
+++ src/main/java/ru/spbstu/icst/problems/Clique.java
@@ -1,8 +1,7 @@
 package ru.spbstu.icst.problems;
 
-import org.jgrapht.alg.clique.BronKerboschCliqueFinder;
-
 import java.util.Scanner;
+import java.util.Set;
 
 public class Clique extends Problem {
     public static final String shortname = "Clique";
@@ -10,36 +9,27 @@ public class Clique extends Problem {
     private final static String description = "For given graph G and number x," +
             " required to find subset of x vertices, all adjacent to each other in the graph.";
 
-    private Scanner scanner = new Scanner(System.in);
-
-    private Graph graph;
-    private BronKerboschCliqueFinder cliqueFinder;
-    private int numVertices;
+    private final Scanner scanner = new Scanner(System.in);
+    private final Graph graph;
+    private Set<Integer> clique;
 
     public Clique() {
         this.graph = new Graph();
     }
 
-    public Clique(int numVertices, Graph graph) {
+    public Clique(Graph graph) {
         this.graph = graph;
-        this.numVertices = numVertices;
-    }
-
-    @Override
-    public String getShortname() {
-        return Clique.shortname;
     }
 
     @Override
     public void readInput() {
         System.out.print("Number of required vertices in Clique: ");
-        this.numVertices = scanner.nextInt();
         this.graph.readGraph();
     }
 
     @Override
     public void solve() {
-
+        this.clique = this.graph.getClique();
     }
 
     @Override
@@ -50,5 +40,10 @@ public class Clique extends Problem {
     @Override
     public void readSolution() {
 
+    }
+
+    @Override
+    public String getShortname() {
+        return Clique.shortname;
     }
 }
