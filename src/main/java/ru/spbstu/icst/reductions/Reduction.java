2ecb0bb4d5b732044d2931842ddad7cb9454cbb9
Ivan <tikerlade@gmail.com>
2022-04-06 03:18:51 +0300

    Provided documentation for Reduction abstract class.

--- src/main/java/ru/spbstu/icst/reductions/Reduction.java
+++ src/main/java/ru/spbstu/icst/reductions/Reduction.java
@@ -2,15 +2,46 @@ package ru.spbstu.icst.reductions;
 
 import ru.spbstu.icst.problems.Problem;
 
+/**
+ * Class which contains all logics required to work with reductions.
+ * Reduction is completed from problem A into problem B.
+ *
+ * Reduction can be completed in 4 different options:
+ * - Forward reduction only;
+ * - Forward reduction + solve reduced problem;
+ * - Forward reduction + solve reduced problem + backward reduction;
+ * - Backward reduction only;
+ *
+ * Forward reduction - convert input of problem A into input of problem B.
+ * Backward reduction - convert solution of problem B into solution of problem A.
+ */
 public abstract class Reduction {
+    /**
+     * Problem which we want to reduce into problem B.
+     */
     Problem problemA;
+
+    /**
+     * Problem into which we want to reduce initial problem A.
+     */
     Problem problemB;
 
+    /**
+     * Constructor with given two exact problems (without data at this point).
+     * @param problemA empty problem A.
+     * @param problemB empty problem B.
+     */
     protected Reduction (Problem problemA, Problem problemB) {
         this.problemA = problemA;
         this.problemB = problemB;
     }
 
+    /**
+     * Entry point into actual algorithm.
+     * From here we decide which parts of reduction we want to complete.
+     * @param programMode mode in which to run program.
+     * @throws Exception if there are problems with parsing input data.
+     */
     public void start(ProgramMode programMode) throws Exception {
         switch (programMode) {
             case FORWARD_ONLY -> {
@@ -36,7 +67,11 @@ public abstract class Reduction {
     }
 
 
-    // Wrapper function for forward reduction
+    /**
+     * Wrapper function for FORWARD_ONLY mode to read user data and after that
+     * execute clean forward reduction.
+     * @throws Exception if there are problems with parsing input data.
+     */
     public void forwardOnly() throws Exception {
         System.out.println("Now you can input data for your " + this.problemA.getShortname() +
                 " problem and you will get output for " + this.problemB.getShortname() + " problem.");
@@ -44,16 +79,31 @@ public abstract class Reduction {
         this.forward();
     }
 
+    /**
+     * Wrapper function for FORWARD_SOLVE mode to read user data and after that
+     * execute clean forward reduction + solve reduced problem.
+     * @throws Exception if there are problems with parsing input data.
+     */
     public void forwardSolve() throws Exception {
         this.forwardOnly();
         this.problemB.solve();
     }
 
+    /**
+     * Wrapper function for FORWARD_SOLVE_BACKWARD mode to read user data and after that
+     * execute clean forward reduction + solve reduced problem + backward reduction.
+     * @throws Exception if there are problems with parsing input data.
+     */
     public void forwardSolveBackward() throws Exception {
         this.forwardSolve();
         this.backward();
     }
 
+    /**
+     * Wrapper function for BACKWARD_ONLY mode to read user data and after that
+     * execute clean backward reduction.
+     * @throws Exception if there are problems with parsing input data.
+     */
     public void backwardOnly() throws Exception {
         System.out.println("Input data for your " + this.problemB.getShortname() + " problem:");
         this.problemB.readInput();
@@ -65,22 +115,22 @@ public abstract class Reduction {
     }
 
     /**
-     * In forward method we know only data for problemA.
-     * And we want to convert this data into data for problemB.
+     * In forward method we know only input data for problemA.
+     * And we want to convert this data into input data for problemB.
      */
     public void forward() {}
 
     /**
-     * In backward method we have solution for problemA,
+     * In backward method we have solution for problemB,
      * and we want to convert it into solution for problemA.
      */
     public void backward() {}
 
     /**
-     * We have algorithm for solving problemB, but we have data for problemA.
-     * Here we will firstly convert data of problemA into data for problemB.
-     * After this we'll solve problemB using known algorithms.
+     * Here we will firstly convert input data of problemA into input data for problemB.
+     * After this we'll solve problemB.
      * When solution for problemB will be found we will convert this solution into solution for problemA.
+     * Case: We have algorithm for solving problemB, but we have input data only for problemA.
      */
     public void forwardAndBackward() throws Exception {}
 
